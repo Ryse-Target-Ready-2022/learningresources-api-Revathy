07d85b24fbbe97cde7d84d2b30818ab46e1e7fc8
Revathy Jayakumar <revathyanu02@gmail.com>
2022-07-15 15:58:55 +0530

    Added JPA and H2 dB

--- learningresources-api/src/main/java/com/targetind/ryse/learningresourcesapi/service/LearningResourceService.java
+++ learningresources-api/src/main/java/com/targetind/ryse/learningresourcesapi/service/LearningResourceService.java
@@ -1,2 +1,44 @@
-package com.targetind.ryse.learningresourcesapi.service;public class LearningResourceService {
+package com.targetind.ryse.learningresourcesapi.service;
+
+import com.targetind.ryse.learningresourcesapi.entity.LearningResource;
+import com.targetind.ryse.learningresourcesapi.repository.LearningResourceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+public class LearningResourceService {
+    private final LearningResourceRepository learningResourceRepository;
+
+    public LearningResourceService(LearningResourceRepository learningResourceRepository) {
+        this.learningResourceRepository = learningResourceRepository;
+    }
+
+    public void saveLearningResources(List<LearningResource> learningResources){
+        for (LearningResource learningResource : learningResources)
+            learningResourceRepository.save(learningResource);
+    }
+
+    public List<LearningResource> getLearningResources(){
+        return learningResourceRepository.findAll();
+    }
+
+    public List<Double> getProfitMargin(){
+        List<LearningResource> learningResources = getLearningResources();
+        List<Double> profitMargins = learningResources.stream().map(lr -> ((lr.getSellingPrice()-lr.getCostPrice())/lr.getSellingPrice())).collect(toList());
+        return profitMargins;
+    }
+
+    public List<LearningResource> sortLearningResourcesByProfitMargin(){
+        List<LearningResource> learningResources = getLearningResources();
+
+        learningResources.sort((lr1,lr2) -> {
+            Double profitMargin1 = (lr1.getSellingPrice() - lr1.getCostPrice())/lr1.getSellingPrice();
+            Double profitMargin2 = (lr2.getSellingPrice() - lr2.getCostPrice())/lr2.getSellingPrice();
+            return profitMargin2.compareTo(profitMargin1);
+        });
+        return learningResources;
+    }
 }
